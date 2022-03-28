5e4827dd1a7e25ec939d9454c293c4fd2432c8d7
Joshua Gehl <jgehl99@gmail.com>
2022-03-28 12:26:11 -0400

    Made navbar sticky, removed dark mode switch, moved cart link into dropdown, added second dropdown that replaces the navbar one when the navbar collapses

--- public_html/content/navbar.php
+++ public_html/content/navbar.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <?php include("../content/headers.php") ?>
-<link rel="stylesheet" type="text/css" href="../dark-mode/dark-mode.css">
+<link rel="stylesheet" type="text/css" href="../css/dark-mode.css">
 <body>
-    <nav class="navbar navbar-expand-md navbar-light bg-light border">
+    <nav class="sticky-top navbar navbar-expand-md navbar-light bg-light border">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../content/index.php">
-            <img src="../static/icon.svg" alt="OldChicken Icon" style="height:50px;width:50px;">
+        <a class="navbar-brand d-flex" href="../content/index.php">
+            <img src="../static/icon.svg" alt="OldChicken Icon" style="height:30px;width:30px;">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -24,26 +24,21 @@
                     <a class="nav-link" href="../content/gallery.php">Gallery</a>
                 </li>
             </ul>
-            <div class="nav-link">
-                <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="darkSwitch">
-                    <label class="custom-control-label" for="darkSwitch">Dark Mode</label>
-                </div>
-            </div>
             <form class="d-flex">
                 <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-primary text-nowrap" type="submit">Search</button>
             </form>
+            <hr class="dropdown-divider mt-sm-3 mt-md-0 d-md-none d-lg-none d-lx-none d-xxl-none">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php if(!isLoggedIn()){ ?>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="../content/login.php">Log In</a>
                     </li>
                 <?php }else { ?>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="../content/cart.php">Cart</a>
-                    </li>
-                    <li class="nav-item dropdown">
+
+
+                    <!-- This list only shows when > sm breakpoint, shows on navbar -->
+                    <li class="nav-item dropdown d-none d-sm-none d-md-block d-lg-block d-lx-block d-xxl-block">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             My Account
                         </a>
@@ -53,15 +48,21 @@
                             <li><a class="dropdown-item" href="../content/logout.php">Log Out</a></li>
                         </ul>
                     </li>
+                    <!-- This list only shows in the collapsed navbar menu when <= sm breakpoint -->
+                    <li class="nav-item dropdown d-block d-sm-block d-md-none">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            My Account
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end border-0 bg-light" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="../content#">Account Homepage</a></li>
+                            <li><a class="dropdown-item" href="../content/cart.php">Cart</a></li>
+                            <li><a class="dropdown-item" href="../content/logout.php">Log Out</a></li>
+                        </ul>
+                    </li>
                 <?php } ?>
             </ul>
         </div>
     </div>
 </nav>
-
-<!--For Dark-mode slider-->
-<script src="../dark-mode/dark-mode.js"></script>
-
-
 </body>
 </html>
