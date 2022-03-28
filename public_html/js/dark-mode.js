67b99387bed1faf2fc13d46144ec26c095f3a74a
Joshua Gehl <jgehl99@gmail.com>
2022-03-28 15:40:31 -0400

    Refactored dark-mode.js to change classes from js instead of css. JS runs before html is loaded to reduce webpage flashing.

--- public_html/js/dark-mode.js
+++ public_html/js/dark-mode.js
@@ -1,36 +1,71 @@
 let darkSwitch = document.getElementById("darkSwitch")
 
-window.addEventListener("load", (
-    function () {
-        if (darkSwitch) {
-            initiate();
-            darkSwitch.addEventListener("change", (
-                function () {
-                    reset();
-                }
-            ))
-        }
-    }
-));
+darkSwitch.checked = localStorage.getItem("data-theme") === "dark";
 
-function initiate()
-{
-    let select = localStorage.getItem("data-theme");
-    if(select){
-        document.body.setAttribute("data-theme", "dark")
-    }
-        // !==null && localStorage.getItem("darkSwitch") === "dark";
-    darkSwitch.checked = select;
+darkSwitch.checked ? applyDarkMode() : applyLightMode();
 
-}
-
-function reset() {
+darkSwitch.onclick = () => {
     if (darkSwitch.checked) {
-        document.body.setAttribute("data-theme", "dark");
-        localStorage.setItem('data-theme', 'dark');
+        applyDarkMode()
+        localStorage.setItem("data-theme", "dark");
     } else {
-        document.body.removeAttribute("data-theme");
-        localStorage.removeItem("data-theme")
+        applyLightMode()
+        localStorage.setItem("data-theme", "light");
     }
 }
 
+function applyDarkMode(){
+    // If dark mode selected:
+
+    // Change all bg-lights to bg-dark
+    document.querySelectorAll('[class*=\'bg-light\']').forEach((e) => {
+        e.className = e.className.replace(/bg-light/g, "bg-dark");
+    });
+
+    // Change all bg-blacks to bg-whites
+    document.querySelectorAll('[class*=\'bg-white\']').forEach((e) => {
+        e.className = e.className.replace(/bg-white/g, "bg-vdark");
+    });
+
+    // Change all text-darks to text-white
+    document.querySelectorAll('[class*=\'text-dark\']').forEach((e) => {
+        e.className = e.className.replace(/text-dark/g, "text-white");
+    });
+
+    // Change all link-darks to link-white
+    document.querySelectorAll('[class*=\'link-dark\']').forEach((e) => {
+        e.className = e.className.replace(/link-dark/g, "link-light");
+    });
+
+    // Set icons to other version
+    document.getElementById("dark_mode_icon").src = "../static/dark_icon.svg";
+    document.getElementById("navbar_icon").src = "../static/hamburger_white.svg";
+}
+
+function applyLightMode(){
+    // If dark mode not selected
+
+    // Change all bg-darks to bg-light
+    document.querySelectorAll('[class*=\'bg-dark\']').forEach((e) => {
+        e.className = e.className.replace(/bg-dark/g, "bg-light");
+    });
+
+    // Change all bg-whites to bg-blacks
+    document.querySelectorAll('[class*=\'bg-vdark\']').forEach((e) => {
+        e.className = e.className.replace(/bg-vdark/g, "bg-white");
+    });
+
+    // Change all text-whites to text-dark
+    document.querySelectorAll('[class*=\'text-white\']').forEach((e) => {
+        e.className = e.className.replace(/text-white/g, "text-dark");
+    });
+
+    // Change all text-whites to text-dark
+    document.querySelectorAll('[class*=\'link-light\']').forEach((e) => {
+        e.className = e.className.replace(/link-light/g, "link-dark");
+    });
+
+    // Set icons to other version
+    document.getElementById("dark_mode_icon").src = "../static/light_icon.svg";
+    document.getElementById("navbar_icon").src = "../static/hamburger_black.svg";
+}
