b9aa2e17e2636c252b91341d170fc36380340902
Joshua Gehl <jgehl99@gmail.com>
2022-03-28 12:24:58 -0400

    Added dark mode css to headers

--- public_html/content/headers.php
+++ public_html/content/headers.php
@@ -18,6 +18,8 @@
     <!-- Ensure $title isn't undefined, if undefined: initialize to empty string-->
     <title> <?php if (empty($title)) $title = ""; echo $title; ?> </title>
 
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -25,6 +27,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="../css/anims.css">
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="../css/dark-mode.css">
 
 </head>
