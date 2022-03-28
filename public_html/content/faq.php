21b529e9194bb1acc8a89986c94a546b1ee71e04
Jarrett Jackson <jrj87@live.ca>
2022-03-27 20:42:52 -0400

    Updated faq page info.

--- public_html/content/faq.php
+++ public_html/content/faq.php
@@ -17,57 +17,58 @@ include('../content/navbar.php');
             </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-3">
-                <div class="card-body"">
-                    <h5 class="card-title">Contact Customer Service</h5>
-                    <h6 class="card-subtitle text-success my-2">605-475-6968 <br>
-                        support@oldchicken.myweb.cs.uwindsor.ca</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-3">
-                <div class="card-body">
-                    <h5 class="card-title">Shipping Fees</h5>
-                    <h6 class="card-subtitle text-success my-2">How much you got?</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-3">
-                <div class="card-body">
-                    <h5 class="card-title">Products too Expensive</h5>
-                    <h6 class="card-subtitle text-success my-2">Suck it up. Chip shortage.</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-3">
-                <div class="card-body">
-                    <h5 class="card-title">Return Policy</h5>
-                    <h6 class="card-subtitle text-success my-2">Not happening.</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-3">
-                <div class="card-body">
-                    <h5 class="card-title">Order Status</h5>
-                    <h6 class="card-subtitle text-success my-2">Whats the rush? It will get there when it does.</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
-            <div class="card h-100 p-2 p-3">
-                <div class="card-body">
-                    <h5 class="card-title">Why is this company so rude?</h5>
-                    <h6 class="card-subtitle text-success my-2">¯\_(ツ)_/¯</h6>
-                </div>
+</div>
+<div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body"">
+                <h5 class="card-title">Contact Customer Service</h5>
+                <h6 class="card-subtitle text-success my-2">605-475-6968 <br>
+                    support@oldchicken.myweb.cs.uwindsor.ca</h6>
             </div>
         </div>
     </div>
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Shipping Fees</h5>
+                <h6 class="card-subtitle text-success my-2">How much you got?</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Products too Expensive</h5>
+                <h6 class="card-subtitle text-success my-2">Suck it up. Chip shortage.</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Return Policy</h5>
+                <h6 class="card-subtitle text-success my-2">Not happening.</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Order Status</h5>
+                <h6 class="card-subtitle text-success my-2">Whats the rush? It will get there when it does.</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-5">
+        <div class="card h-100 p-2 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Why is this company so rude?</h5>
+                <h6 class="card-subtitle text-success my-2">¯\_(ツ)_/¯</h6>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 </body>
