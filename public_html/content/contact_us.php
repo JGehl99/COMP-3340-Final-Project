2c29a806367d0a458b5fbcb814f06c69bddebc83
Jarrett Jackson <jrj87@live.ca>
2022-03-27 21:10:17 -0400

    Adjusted col's for each container in faq.php as well as added the container for contact_us.php and set col's accordingly.

--- public_html/content/contact_us.php
+++ public_html/content/contact_us.php
@@ -8,4 +8,27 @@ include('navbar.php');
 
 <body></body>
 
+<div class="container flex py-5 h-100" >
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-8 col-md-6 col-lg-4 col-xl-3" style="max-width: 25rem;">
+            <div class="card" style="border-radius: 1rem;">
+                <div class="card-body p-3 text-center">
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-12 col-sm-12 col-md-10 col-lg-6 col-xl-5 col-xxl-4 mt-5">
+        <div class="card h-100 p-3">
+            <div class="card-body"">
+            <h5 class="card-title">Contact Customer Service</h5>
+            <h6 class="card-subtitle text-success my-2">Phone support: <a href="tel:605-475-6968">605-475-6968</a><br>
+                Email: <a href="mailto:<support@oldchicken.myweb.cs.uwindsor.ca? subject=subject text">support@oldchicken.myweb.cs.uwindsor.ca</a> </h6>
+        </div>
+    </div>
+</div>
+
+
 </html>
