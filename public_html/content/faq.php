e76c21bae63182b0eaf5070651b81d286e0a746a
Brady Malott <brady.malott@gmail.com>
2022-03-30 20:48:25 -0400

    Replaced inline styles with bootstrap classes

--- public_html/content/faq.php
+++ public_html/content/faq.php
@@ -12,66 +12,67 @@
 <?php include('navbar.php'); ?>
 <div class="container flex py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-6 col-md-4 col-lg-2" style="max-width: 25rem;">
-            <div class="card bg-light text-dark rounded-5" style="border-radius: 1rem;">
+        <div class="col-6 col-md-4 col-lg-2">
+            <div class="card bg-light text-dark rounded-5">
                 <div class="card-body p-3 text-center">
                     <h1>FAQ</h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="row d-flex justify-content-center align-items-center h-100">
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Contact Customer Service</h5>
-                <h6 class="card-subtitle text-success my-2">Phone support: <a
-                            href="tel:605-475-6968">605-475-6968</a><br>
-                    Email: <a href="mailto:<support@oldchicken.myweb.cs.uwindsor.ca? subject=subject text">support@oldchicken.myweb.cs.uwindsor.ca</a>
-                </h6>
+
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Contact Customer Service</h5>
+                    <h6 class="card-subtitle text-success my-2 text-nowrap">Phone support: <a
+                                href="tel:605-475-6968">605-475-6968</a><br>
+                        Email: <a href="mailto:<support@oldchicken.myweb.cs.uwindsor.ca? subject=subject text">support@oldchicken.myweb.cs.uwindsor.ca</a>
+                    </h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Shipping Fees</h5>
-                <h6 class="card-subtitle text-success my-2">How much you got?<br>
-                    <a href="shipping_returns.php">Click here to learn more</a></h6>
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Shipping Fees</h5>
+                    <h6 class="card-subtitle text-success my-2">How much you got?<br>
+                        <a href="shipping_returns.php">Click here to learn more</a></h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Products too Expensive</h5>
-                <h6 class="card-subtitle text-success my-2">Suck it up. Chip shortage.</h6><br>
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Products too Expensive</h5>
+                    <h6 class="card-subtitle text-success my-2">Suck it up. Chip shortage.</h6><br>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Return Policy</h5>
-                <h6 class="card-subtitle text-success my-2">Not happening.<br>
-                    <a href="shipping_returns.php">Click here to learn more</a></h6>
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Return Policy</h5>
+                    <h6 class="card-subtitle text-success my-2">Not happening.<br>
+                        <a href="shipping_returns.php">Click here to learn more</a></h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Order Status</h5>
-                <h6 class="card-subtitle text-success my-2">Whats the rush? It will get there when it does.</h6><br>
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Order Status</h5>
+                    <h6 class="card-subtitle text-success my-2">Whats the rush? It will get there when it does.</h6><br>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
-        <div class="card h-100 p-2 p-3 bg-light text-dark rounded-5">
-            <div class="card-body">
-                <h5 class="card-title">Why is this company so rude?</h5>
-                <h6 class="card-subtitle text-success my-2">¯\_(ツ)_/¯</h6><br>
+        <div class="col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 col-xxl-4 mt-5">
+            <div class="card h-100 p-2 p-3 bg-light text-dark rounded-5">
+                <div class="card-body">
+                    <h5 class="card-title">Why is this company so rude?</h5>
+                    <h6 class="card-subtitle text-success my-2">¯\_(ツ)_/¯</h6><br>
+                </div>
             </div>
         </div>
     </div>
