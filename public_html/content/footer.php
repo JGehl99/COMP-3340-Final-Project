5727dcb90ba49a4fb735a08d6136bced84937fbc
Joshua Gehl <jgehl99@gmail.com>
2022-03-30 23:23:38 -0400

    Added check to ensure switch is clicked on page load if theme is set to dark

--- public_html/content/footer.php
+++ public_html/content/footer.php
@@ -1,25 +1,25 @@
 <!-- Footer -->
-<footer class="text-center bg-light">
-    <div class="container p-4">
+<footer class="text-center background">
+    <div class="container p-4 mt-5">
         <section class="">
-            <div class="row text-dark">
+            <div class="row text-color">
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <h5 class="text-uppercase">About Us</h5>
                     <ul class="list-unstyled mb-0">
                         <li>
-                            <a href="about_us.php#joshua_gehl" class="link-dark">Joshua Gehl</a>
+                            <a href="about_us.php#joshua_gehl" class="link-color">Joshua Gehl</a>
                         </li>
                         <li>
-                            <a href="about_us.php#jarrett_jackson" class="link-dark">Jarrett Jackson</a>
+                            <a href="about_us.php#jarrett_jackson" class="link-color">Jarrett Jackson</a>
                         </li>
                         <li>
-                            <a href="about_us.php#brady_malott" class="link-dark">Brady Malott</a>
+                            <a href="about_us.php#brady_malott" class="link-color">Brady Malott</a>
                         </li>
                         <li>
-                            <a href="about_us.php#jonathan_fehr" class="link-dark">Jonathan Fehr</a>
+                            <a href="about_us.php#jonathan_fehr" class="link-color">Jonathan Fehr</a>
                         </li>
                         <li>
-                            <a href="about_us.php#shady_gerges" class="link-dark">Shady Gerges</a>
+                            <a href="about_us.php#shady_gerges" class="link-color">Shady Gerges</a>
                         </li>
                     </ul>
                 </div>
@@ -27,13 +27,13 @@
                     <h5 class="text-uppercase">Customer Support</h5>
                     <ul class="list-unstyled mb-0">
                         <li>
-                            <a href="faq.php" class="link-dark">F.A.Q.</a>
+                            <a href="faq.php" class="link-color">F.A.Q.</a>
                         </li>
                         <li>
-                            <a href="shipping_returns.php" class="link-dark">Shipping & Returns</a>
+                            <a href="shipping_returns.php" class="link-color">Shipping & Returns</a>
                         </li>
                         <li>
-                            <a href="contact_us.php" class="link-dark">Contact Us</a>
+                            <a href="contact_us.php" class="link-color">Contact Us</a>
                         </li>
                     </ul>
                 </div>
@@ -41,10 +41,10 @@
                     <h5 class="text-uppercase">Sales</h5>
                     <ul class="list-unstyled mb-0">
                         <li>
-                            <a href="products.php" class="link-dark">Products</a>
+                            <a href="products.php" class="link-color">Products</a>
                         </li>
                         <li>
-                            <a href="gallery.php" class="link-dark">Gallery</a>
+                            <a href="gallery.php" class="link-color">Gallery</a>
                         </li>
                     </ul>
                 </div>
@@ -55,17 +55,17 @@
                         <?php
                         if (!isLoggedIn()) { ?>
                             <li>
-                                <a href="login.php" class="link-dark">Log In</a>
+                                <a href="login.php" class="link-color">Log In</a>
                             </li>
                         <?php } else { ?>
                             <li>
-                                <a href="#" class="link-dark">My Account</a>
+                                <a href="#" class="link-color">My Account</a>
                             </li>
                             <li>
-                                <a href="cart.php" class="link-dark">Cart</a>
+                                <a href="cart.php" class="link-color">Cart</a>
                             </li>
                             <li>
-                                <a href="logout.php" class="link-dark">Log Out</a>
+                                <a href="logout.php" class="link-color">Log Out</a>
                             </li>
                         <?php } ?>
                     </ul>
@@ -73,13 +73,21 @@
             </div>
         </section>
     </div>
-    <div class="text-center p-3 bg-light d-flex justify-content-start">
-        <div class="form-check form-switch">
-            <input type="checkbox" class="form-check-input" id="darkSwitch">
+    <div class="text-center p-3 background d-flex justify-content-start">
+        <form class="form-check form-switch" id="theme-form">
+            <input type="checkbox" class="form-check-input" id="darkSwitch" onchange="refreshCSS()"
+                <?php
+                if (isset($_SESSION['theme-var'])) { ?>
+                    <?php
+                    if ($_SESSION['theme-var'] == 'dark') {
+                        echo ' checked';
+                    }
+                }
+                ?>>
             <label class="form-check-label" for="darkSwitch">
                 <img id="dark_mode_icon" src="../static/light_icon.svg" alt="Icon for light mode">
             </label>
-        </div>
+        </form>
     </div>
 </footer>
 
