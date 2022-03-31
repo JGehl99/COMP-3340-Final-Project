86131b570fe2c5f55fa14133aad8448b89737656
Jonathan Fehr <fehr113@uwindsor.ca>
2022-03-31 16:36:55 -0400

    cleaning up gallery images

--- public_html/content/gallery.php
+++ public_html/content/gallery.php
@@ -26,7 +26,7 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #1</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_1.jpeg" alt="Custom PC #1">
+                <img class="rounded img-fluid" src="../static/custom_build_1.jpeg" alt="Custom PC #1">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
                     CPU - AMD Ryzen 5 3600<br>
@@ -38,7 +38,7 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #2</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_2.jpg" alt="Custom PC #2">
+                <img class="rounded img-fluid" src="../static/custom_build_2.jpeg" alt="Custom PC #2">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
                     CPU - AMD Ryzen Threadripper 3970X<br>
@@ -50,11 +50,11 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #3</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_3.jpeg" alt="Custom PC #3">
+                <img class="rounded img-fluid" src="../static/custom_build_3.jpeg" alt="Custom PC #3">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
-                    CPU - Intel Core i9-9900KF<br>
-                    GPU - EVGA Geforce GTX 1080 TI</h6>
+                    CPU - Intel Core i7-7660U<br>
+                    GPU - EVGA Geforce RTX 2080 TI</h6>
             </div>
         </div>
     </div>
@@ -62,7 +62,7 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #4</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_4.jpeg" alt="Custom PC #4">
+                <img class="rounded img-fluid" src="../static/custom_build_4.jpeg" alt="Custom PC #4">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
                     CPU - Intel Core i5-9600K<br>
@@ -74,11 +74,11 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #5</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_5.png" alt="Custom PC #5">
+                <img class="rounded img-fluid" src="../static/custom_build_5.jpeg" alt="Custom PC #5">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
-                    CPU - AMD Ryzen 7 5800X<br>
-                    GPU - EVGA Geforce RTX 2080</h6>
+                    CPU - AMD Ryzen 5 3600<br>
+                    GPU - MSI Geforce GTX 1660 Super</h6>
             </div>
         </div>
     </div>
@@ -86,11 +86,11 @@
         <div class="card h-100 p-3 background text-color rounded-5 gallery-card">
             <div class="card-body">
                 <h5 class="card-title">Custom PC Build #6</h5>
-                <img class="rounded img-fluid" src="../media/custom_build_6.jpg" alt="Custom PC #6">
+                <img class="rounded img-fluid" src="../static/custom_build_6.jpeg" alt="Custom PC #6">
                 <h5 class="card-title"><br>Parts List:</h5>
                 <h6 class="card-subtitle text-success my-2">
-                    CPU - AMD Ryzen 5 3600<br>
-                    GPU - MSI Geforce GTX 1660 Super</h6>
+                    CPU - AMD Ryzen 7 5800X<br>
+                    GPU - EVGA Geforce RTX 2080</h6>
             </div>
         </div>
     </div>
