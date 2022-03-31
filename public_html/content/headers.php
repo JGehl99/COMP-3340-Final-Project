9c531106ba7198a46aa38ea785623e08e80bf18c
Joshua Gehl <jgehl99@gmail.com>
2022-03-31 15:47:16 -0400

    Updated php to set default theme

--- public_html/content/headers.php
+++ public_html/content/headers.php
@@ -45,6 +45,7 @@ include_once("../static/config.php");
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <link rel="stylesheet" href="../css/stylesheet.css">
+<link rel="stylesheet" href="../css/bttStyle.css">
 
 <?php
 
@@ -56,5 +57,6 @@ if (isset($_SESSION['theme-var'])) {
     }
 } else {
     echo '<link id="theme-stylesheet" rel="stylesheet" href="../css/light.css">';
+    $_SESSION['theme-var'] = 'light';
 }
 ?>
