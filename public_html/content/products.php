1c4a739284fd1bbdbb37f3881048542f851ddc63
Brady Malott <brady.malott@gmail.com>
2022-04-04 15:45:38 -0400

    Fixed attribute not having data- before it

--- public_html/content/products.php
+++ public_html/content/products.php
@@ -42,8 +42,9 @@ $conn->close();
 
             // Create the card for the item ?>
             <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 pt-5">
-                <a href="#" class="card h-100 hover_expand cursor_pointer text-decoration-none background text-color product-link shadow"
-                   item-id="<?php echo $id; ?>">
+                <a href="#"
+                   class="card h-100 hover_expand cursor_pointer text-decoration-none background text-color product-link shadow"
+                   data-item-id="<?php echo $id; ?>">
                     <img class="card-img-top p-4" src="<?php echo $imageURL; ?>" alt="<?php echo $name; ?>"/>
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $name; ?></h5>
